39774071a6f330b09828029b12bff11aabf56ba1
euclideands <nabhannabilah2003@students.unnes.ac.id>
2023-11-15 21:09:42 +0700

    fixed multi page

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
 //DOM Elements
 const circles = document.querySelectorAll(".circle"),
   progressBar = document.querySelector(".indicator"),
-  buttons = document.querySelectorAll(".buttons");
+  buttons = document.querySelectorAll("button");
 
   // console.log(circles, progressBar, buttons);
 
-  let currentStep = 1;
+let currentStep = 1;
 
-  const updateSteps = (e) => {
-    currentStep = e.target.id === "next" ? ++ currentStep : --currentStep;
-    circles.forEach((circle,index) => {
-      circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");
-    });
-    progressBar.style.width = `${((currentStep - 1) / (circles.length - 1)) * 100}%`
-  };
+const updateSteps = (e) => {
+  currentStep = e.target.id === "next" ? ++ currentStep : --currentStep;
+  circles.forEach((circle,index) => {
+    circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");
+  });
+
+  progressBar.style.width = `${((currentStep - 1) / (circles.length - 1)) * 100}%`;
+
+  if (currentStep === circles.length) {
+    buttons[1].disabled = true;
+  } else if (currentStep === 1) {
+    buttons[0].disabled = true;
+  } else {
+    buttons.forEach((button) => (button.disabled = false));
+  }
+};
 
 buttons.forEach((button) => {
   button.addEventListener("click", updateSteps)
 });
-
-
-
-
-
-// let currentStep = 1;
-
-// // function that updates the current step and updates the DOM
-// const updateSteps = (e) => {
-//   // update current step based on the button clicked
-//   currentStep = e.target.id === "next" ? ++ currentStep : --currentStep;
-//   // loop 
-//   circles.forEach((circle, index) => {
-//     circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");
-//   });
-// };
-  
-// // add click event listeners to all buttons
-// buttons.forEach((button) => {
-//   button.addEventListener("click", updateSteps)
-// });
